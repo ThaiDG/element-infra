d476a0c2361a4724509037351e6b6bc79c59fcf3
Đổng Giang Thái <thaidg@tapofthink.com>
2025-08-05 20:25:03 +0700

    Create database proxy so we can use it to pooling the connection and local connection

--- database/main.tf
+++ database/main.tf
@@ -69,10 +69,98 @@ resource "aws_rds_cluster_instance" "aurora_pg_instance" {
   }
 }
 
+resource "aws_db_proxy" "aurora_pg_proxy" {
+  name                   = "${var.workspace}-aurora-pg-proxy"
+  engine_family          = "POSTGRESQL"
+  role_arn               = aws_iam_role.rds_proxy_role.arn
+  vpc_subnet_ids         = data.terraform_remote_state.vpc.outputs.private_subnet_ids
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  require_tls            = false
+
+  auth {
+    auth_scheme = "SECRETS"
+    secret_arn  = aws_secretsmanager_secret.db_credentials.arn
+    iam_auth    = "DISABLED"
+  }
+
+  tags = {
+    Name = "${var.workspace}-aurora-pg-proxy"
+  }
+}
+
+resource "aws_db_proxy_default_target_group" "aurora_pg_proxy_tg" {
+  db_proxy_name = aws_db_proxy.aurora_pg_proxy.name
+}
+
+resource "aws_db_proxy_target" "aurora_pg_proxy_target" {
+  db_proxy_name         = aws_db_proxy.aurora_pg_proxy.name
+  target_group_name     = aws_db_proxy_default_target_group.aurora_pg_proxy_tg.name
+  db_cluster_identifier = aws_rds_cluster.aurora_pg.id
+}
+
+resource "aws_iam_role" "rds_proxy_role" {
+  name = "${var.workspace}-rds-proxy-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "rds.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "rds_proxy_custom_policy" {
+  name        = "${var.workspace}-rds-proxy-custom-policy"
+  description = "Custom policy for RDS Proxy service role"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret",
+          "secretsmanager:ListSecretVersionIds"
+        ],
+        Resource = "${aws_secretsmanager_secret.db_credentials.arn}"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "kms:Decrypt"
+        ],
+        Resource = "*" # You can scope this to your KMS key if needed
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "rds_proxy_policy" {
+  role       = aws_iam_role.rds_proxy_role.name
+  policy_arn = aws_iam_policy.rds_proxy_custom_policy.arn
+}
+
+resource "aws_secretsmanager_secret" "db_credentials" {
+  name = "${var.workspace}-aurora-db-credentials"
+}
+
+resource "aws_secretsmanager_secret_version" "db_credentials_version" {
+  secret_id = aws_secretsmanager_secret.db_credentials.id
+  secret_string = jsonencode({
+    username = "${data.aws_ssm_parameter.db_username.value}"
+    password = "${data.aws_ssm_parameter.db_password.value}"
+  })
+}
+
 resource "aws_route53_record" "aurora_pg_dns" {
   zone_id = data.aws_route53_zone.main.id
   name    = "postgres.${var.root_domain}"
   type    = "CNAME"
   ttl     = 300
-  records = [aws_rds_cluster.aurora_pg.endpoint]
+  records = [aws_db_proxy.aurora_pg_proxy.endpoint]
 }
