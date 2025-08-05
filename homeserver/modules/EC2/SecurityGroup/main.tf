47dd34ececb16919cf71d64a72f752b213bd0228
Đổng Giang Thái <thaidg@tapofthink.com>
2025-08-05 14:06:51 +0700

    Refactoring

--- homeserver/modules/EC2/SecurityGroup/main.tf
+++ homeserver/modules/EC2/SecurityGroup/main.tf
@@ -14,13 +14,13 @@ resource "aws_security_group" "security_group" {
   dynamic "ingress" {
     for_each = var.ingress_rules
     content {
-      description     = "${ingress.value.description}"
-      from_port       = "${ingress.value.from_port}"
-      to_port         = "${ingress.value.to_port}"
-      protocol        = "${ingress.value.protocol}"
-      cidr_blocks     = "${ingress.value.cidr_blocks}"
+      description     = ingress.value.description
+      from_port       = ingress.value.from_port
+      to_port         = ingress.value.to_port
+      protocol        = ingress.value.protocol
+      cidr_blocks     = ingress.value.cidr_blocks
       self            = true
-      security_groups = "${ingress.value.security_groups}"
+      security_groups = ingress.value.security_groups
     }
   }
 
--- homeserver/modules/EC2/SecurityGroup/variables.tf
+++ homeserver/modules/EC2/SecurityGroup/variables.tf
@@ -16,38 +16,12 @@ variable "vpc_id" {
 variable "ingress_rules" {
   description = "List of ingress rules for the security group"
   type = list(object({
-    description = string
-    from_port   = number
-    to_port     = number
-    protocol    = string
-    cidr_blocks = list(string)
+    description     = string
+    from_port       = number
+    to_port         = number
+    protocol        = string
+    cidr_blocks     = list(string)
     security_groups = optional(list(string), [])
   }))
   default = []
 }
-
-# variable "ingress_description" {
-#   description = "Description for the ingress rule"
-#   type        = string
-# }
-
-# variable "ingress_from_port" {
-#   description = "Starting port for the ingress rule"
-#   type        = number
-# }
-
-# variable "ingress_to_port" {
-#   description = "Ending port for the ingress rule"
-#   type        = number
-# }
-
-# variable "ingress_protocol" {
-#   description = "Protocol for the ingress rule"
-#   type        = string
-# }
-
-# variable "ingress_cidr_blocks" {
-#   description = "CIDR blocks for the ingress rule"
-#   type        = list(string)
-#   default     = ["10.0.0.0/16"]   # VPC CIDR block
-# }
