2fbd860699006e7873b5614577bdada142b4d1f8
Đổng Giang Thái <thaidg@tapofthink.com>
2025-08-05 20:26:15 +0700

    Add the VPC endpoint, prepare for future security

--- vpc/main.tf
+++ vpc/main.tf
@@ -17,3 +17,106 @@ resource "aws_internet_gateway" "igw" {
     Name = "${var.workspace}-element-igw"
   }
 }
+
+# VPC Interface Endpoints
+# This will allow SSM connecting to the EC2 instance in private subnet
+
+# Security group that allow HTTPS outbound
+resource "aws_security_group" "vpc_endpoint_sg" {
+  name        = "${var.workspace}-vpc-endpoint-sg"
+  description = "Allow HTTPS traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ssm"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  subnet_ids = [
+    module.private_subnet_1.subnet_id,
+    module.private_subnet_2.subnet_id,
+    module.private_subnet_3.subnet_id
+  ]
+  security_group_ids = [aws_security_group.vpc_endpoint_sg.id]
+
+  tags = {
+    Name = "${var.workspace}-ssm-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "ec2messages" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ec2messages"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  subnet_ids = [
+    module.private_subnet_1.subnet_id,
+    module.private_subnet_2.subnet_id,
+    module.private_subnet_3.subnet_id
+  ]
+  security_group_ids = [aws_security_group.vpc_endpoint_sg.id]
+
+  tags = {
+    Name = "${var.workspace}-ec2messages-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "ssmmessages" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ssmmessages"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  subnet_ids = [
+    module.private_subnet_1.subnet_id,
+    module.private_subnet_2.subnet_id,
+    module.private_subnet_3.subnet_id
+  ]
+  security_group_ids = [aws_security_group.vpc_endpoint_sg.id]
+
+  tags = {
+    Name = "${var.workspace}-ssmmessages-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "ec2" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.ec2"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  subnet_ids = [
+    module.private_subnet_1.subnet_id,
+    module.private_subnet_2.subnet_id,
+    module.private_subnet_3.subnet_id
+  ]
+  security_group_ids = [aws_security_group.vpc_endpoint_sg.id]
+
+  tags = {
+    Name = "${var.workspace}-ec2-endpoint"
+  }
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.main.id
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = [aws_route_table.private.id]
+
+  tags = {
+    Name = "${var.workspace}-s3-endpoint"
+  }
+}
